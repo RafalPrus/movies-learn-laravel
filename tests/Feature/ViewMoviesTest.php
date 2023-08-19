3b910c211188fafeae2cb6ccb66f0fb9c05e70ea
Rafał Pruś <rafalprues@gmail.com>
2023-08-19 13:17:16 +0200

    Add single movie page view tests with fake data in response

--- tests/Feature/ViewMoviesTest.php
+++ tests/Feature/ViewMoviesTest.php
@@ -4,11 +4,12 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Http;
+use Nette\Utils\Random;
 use Tests\TestCase;
 
 class ViewMoviesTest extends TestCase
 {
-    public function testMainPageReturnSuccessfulResponse (): void
+    public function testMainPageShowsCorrectInfo (): void
     {
         Http::fake([
             'https://api.themoviedb.org/3/movie/popular' => $this->fakePopularMovies(),
@@ -29,6 +30,25 @@ class ViewMoviesTest extends TestCase
         $response->assertSeeText('Now Playing');
         $response->assertSeeText('Now Playing Fake Movie');
         $response->assertSeeTextInOrder(['Adventure', 'Drama', 'Mystery', 'Science Fiction', 'Thriller']);
+
+    }
+
+    public function testMoviePageShowsCorrectInfo(): void
+    {
+        Http::fake([
+            "https://api.themoviedb.org/3/movie/*" => $this->fakeSingleMovie()
+        ]);
+
+        $response = $this->get(route('movies.show', Random::generate(4, 0-9)));
+
+        $response->assertSeeText('Featured Crew');
+        $response->assertSeeText('Play Trailer');
+        $response->assertSeeText('Fake Jumanji: The Next Level');
+        $response->assertSeeText('Dr. Smolder Bravestone');
+        $response->assertSee('rBxcF-r9Ibs');
+        $response->assertSee('/bB42KDdfWkOvmzmYkmK58ZlCa9P.jpg');
+        $response->assertSeeTextInOrder(['Adventure', 'Drama', 'Mystery', 'Science Fiction', 'Thriller']);
+
 
     }
 
@@ -172,6 +192,85 @@ class ViewMoviesTest extends TestCase
                     "id" => 37,
                     "name" => "Western"
                 ],
+            ]
+        ], 200);
+    }
+
+    public function fakeSingleMovie()
+    {
+        return Http::response([
+            "adult" => false,
+            "backdrop_path" => "/hreiLoPysWG79TsyQgMzFKaOTF5.jpg",
+            "genres" => [
+                ["id" => 28, "name" => "Action"],
+                ["id" => 12, "name" => "Adventure"],
+                ["id" => 35, "name" => "Comedy"],
+                ["id" => 14, "name" => "Fantasy"],
+            ],
+            "homepage" => "http://jumanjimovie.com",
+            "id" => 12345,
+            "overview" => "As the gang return to Jumanji to rescue one of their own, they discover that nothing is as they expect. The players will have to brave parts unknown and unexplored.",
+            "poster_path" => "/bB42KDdfWkOvmzmYkmK58ZlCa9P.jpg",
+            "release_date" => "2019-12-04",
+            "runtime" => 123,
+            "title" => "Fake Jumanji: The Next Level",
+            "vote_average" => 6.8,
+            "credits" => [
+                "cast" => [
+                    [
+                        "cast_id" => 2,
+                        "character" => "Dr. Smolder Bravestone",
+                        "credit_id" => "5aac3960c3a36846ea005147",
+                        "gender" => 2,
+                        "id" => 18918,
+                        "name" => "Dwayne Johnson",
+                        "order" => 0,
+                        "profile_path" => "/kuqFzlYMc2IrsOyPznMd1FroeGq.jpg",
+                    ]
+                ],
+                "crew" => [
+                    [
+                        "credit_id" => "5d51d4ff18b75100174608d8",
+                        "department" => "Production",
+                        "gender" => 1,
+                        "id" => 546,
+                        "job" => "Casting Director",
+                        "name" => "Jeanne McCarthy",
+                        "profile_path" => null,
+                    ]
+                ]
+            ],
+            "videos" => [
+                "results" => [
+                    [
+                        "id" => "5d1a1a9b30aa3163c6c5fe57",
+                        "iso_639_1" => "en",
+                        "iso_3166_1" => "US",
+                        "key" => "rBxcF-r9Ibs",
+                        "name" => "JUMANJI: THE NEXT LEVEL - Official Trailer (HD)",
+                        "site" => "YouTube",
+                        "size" => 1080,
+                        "type" => "Trailer",
+                    ]
+                ]
+            ],
+            "images" => [
+                "backdrops" => [
+                    [
+                        "aspect_ratio" => 1.7777777777778,
+                        "file_path" => "/hreiLoPysWG79TsyQgMzFKaOTF5.jpg",
+                        "height" => 2160,
+                        "iso_639_1" => null,
+                        "vote_average" => 5.388,
+                        "vote_count" => 4,
+                        "width" => 3840,
+                    ]
+                ],
+                "posters" => [
+                    [
+
+                    ]
+                ]
             ]
         ], 200);
     }
