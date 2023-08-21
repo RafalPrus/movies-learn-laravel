625e895153276135ecb6ea848567dc00d5aee882
Rafał Pruś <rafalprues@gmail.com>
2023-08-21 10:50:36 +0200

    Refactor loops in show view for better performance

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -31,6 +31,8 @@
                                     <div>{{ $crew['name'] }}</div>
                                     <div class="text-sm text-gray-400">{{ $crew['job'] }}</div>
                                 </div>
+                            @else
+                                @break
                             @endif
                         @endforeach
                     </div>
@@ -81,18 +83,22 @@
         <div class="container mx-auto px-4 py-16">
             <h2 class="text-4xl font-semibold">Cast</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-                @foreach(array_slice($movie['credits']['cast'], 0, 5) as $cast)
-                <div class="mt-8">
-                    <a href="#">
-                        <img src="https://image.tmdb.org/t/p/w300/{{ $cast['profile_path'] }}" alt="parasite" class="hover:opacity-75 transition ease-in-out">
-                    </a>
-                    <div class="mt-2">
-                        <a href="#" class="text-lg mt-2 hover:text-gray:300">{{ $cast['original_name'] }}</a>
-                        <div class="text-sm text-gray-400">
-                            {{ $cast['character'] }}
+                @foreach($movie['credits']['cast'] as $cast)
+                    @if($loop->index < 5)
+                        <div class="mt-8">
+                            <a href="#">
+                                <img src="https://image.tmdb.org/t/p/w300/{{ $cast['profile_path'] }}" alt="parasite" class="hover:opacity-75 transition ease-in-out">
+                            </a>
+                            <div class="mt-2">
+                                <a href="#" class="text-lg mt-2 hover:text-gray:300">{{ $cast['original_name'] }}</a>
+                                <div class="text-sm text-gray-400">
+                                    {{ $cast['character'] }}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    @else
+                        @break
+                    @endif
                 @endforeach
             </div>
         </div>
