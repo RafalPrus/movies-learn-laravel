09519668f96f584e99f93543a3b49bb22d22bbe7
Rafał Pruś <rafalprues@gmail.com>
2023-08-21 08:45:03 +0200

    Add tests of livewire search dropdown component

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>My movie app</title>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite('resources/css/app.css')
     @livewireStyles
 </head>
